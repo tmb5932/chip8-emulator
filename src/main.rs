c2ff14521549ed4233e20a2f2787f1034964a2d2
tmb5932 <tmb5932@g.rit.edu>
2025-06-14 13:09:58 -0400

    updated variables to proper naming convention

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeTo;
+
 use minifb::{Window, WindowOptions};
 use rand::{RngCore, SeedableRng};
 use rand_chacha::ChaCha8Rng;
@@ -78,22 +80,21 @@ impl Chip8 {
     }
 
     pub fn cycle(&mut self) -> std::io::Result<()> {
-        // Infinitely loop
         // Fetch
         let opcode: u16 = (self.memory[self.pc as usize] as u16) << 8 |  self.memory[(self.pc + 1) as usize] as u16;
         self.pc += 2;
         
         // Decode
         // X: The second nibble. Used to look up one of the 16 registers (VX) from V0 through VF.
-        let X: usize = ((opcode & 0x0F00) >> 8) as usize;
+        let x: usize = ((opcode & 0x0F00) >> 8) as usize;
         // Y: The third nibble. Also used to look up one of the 16 registers (VY) from V0 through VF.
-        let Y: usize = ((opcode & 0x00F0) >> 4) as usize;
+        let y: usize = ((opcode & 0x00F0) >> 4) as usize;
         // N: The fourth nibble. A 4-bit number.
-        let N: u8 = (opcode & 0x000F) as u8;
+        let n: u8 = (opcode & 0x000F) as u8;
         // NN: The second byte (third and fourth nibbles). An 8-bit immediate number.
-        let NN: u8 = (opcode & 0x00FF) as u8;
+        let nn: u8 = (opcode & 0x00FF) as u8;
         // NNN: The second, third and fourth nibbles. A 12-bit immediate memory address.
-        let NNN: u16 = opcode & 0x0FFF;
+        let nnn: u16 = opcode & 0x0FFF;
 
         // Execute
         match opcode & 0xF000 {
@@ -110,90 +111,90 @@ impl Chip8 {
             }
             0x1000 => {
                 // Jump: PC = NNN
-                self.pc = NNN;
+                self.pc = nnn;
             }
             0x2000 => {
                 // JAL: STACK[SP] = PC & PC = NNN
                 self.stack[self.sp] = self.pc;
                 self.sp += 1;
-                self.pc = NNN;
+                self.pc = nnn;
             }
             0x3000 => {
                 // Skip 1 instruction if VX == NN
-                if self.v[X] == NN {
+                if self.v[x] == nn {
                     self.pc += 2;
                 }
             }
             0x4000 => {
                 // Skip 1 instruction if VX != NN
-                if self.v[X] != NN {
+                if self.v[x] != nn {
                     self.pc += 2;
                 }
             }
             0x5000 => {
                 // Skip 1 instruction if VX == VY
-                if self.v[X] == self.v[Y] {
+                if self.v[x] == self.v[y] {
                     self.pc += 2;
                 }
             }
             0x6000 => {
                 // SET: VX = NN
-                self.v[X] = NN;
+                self.v[x] = nn;
             }
             0x7000 => {
                 // ADD: VX = VX + NN
-                self.v[X] += NN;
+                self.v[x] += nn;
             }
             0x8000 => {
                 match opcode & 0x000F {
                     // BINARY OPS
                     0x0000 => {
                         // SET: VX = VY
-                        self.v[X] = self.v[Y];
+                        self.v[x] = self.v[y];
                     }
                     0x0001 => {
                         // OR: VX = VX OR VY
-                        self.v[X] = self.v[X] | self.v[Y];
+                        self.v[x] = self.v[x] | self.v[y];
                     }
                     0x0002 => {
                         // AND: VX = VX AND VY
-                        self.v[X] = self.v[X] & self.v[Y];
+                        self.v[x] = self.v[x] & self.v[y];
                     }
                     0x0003 => {
                         // XOR: VX = VX XOR VY
-                        self.v[X] = self.v[X] ^ self.v[Y];
+                        self.v[x] = self.v[x] ^ self.v[y];
                     }
                     0x0004 => {
                         // ADD (with overflow): VX = VX + VY
-                        let (sum, carry) = self.v[X].overflowing_add(self.v[Y]);
-                        self.v[X] = sum;
+                        let (sum, carry) = self.v[x].overflowing_add(self.v[y]);
+                        self.v[x] = sum;
                         self.v[0xF] = if carry { 1 } else { 0 };
                     }
                     0x0005 => {
                         // 8XY5 sets VX to the result of VX - VY.                        
-                        let (result, borrowed) = self.v[X].overflowing_sub(self.v[Y]);
-                        self.v[X] = result;
+                        let (result, borrowed) = self.v[x].overflowing_sub(self.v[y]);
+                        self.v[x] = result;
                         self.v[0xF] = if borrowed { 0 } else { 1 };
                     }
                     0x0006 => {
                         // YSHIFT:    8XY6 VX = VY >> 1
                         // No YSHIFT: 8XY6 VX = VX >> 1
-                        let shift_src = if self.super_chip_support { X } else { Y };
+                        let shift_src = if self.super_chip_support { x } else { y };
                         self.v[0xF] = self.v[shift_src] >> 7;
-                        self.v[X] = self.v[shift_src] >> 1;
+                        self.v[x] = self.v[shift_src] >> 1;
                     }
                     0x0007 => {
                         // 8XY7 sets VX to the result of VY - VX.
-                        let (result, borrowed) = self.v[Y].overflowing_sub(self.v[X]);
-                        self.v[X] = result;
+                        let (result, borrowed) = self.v[y].overflowing_sub(self.v[x]);
+                        self.v[x] = result;
                         self.v[0xF] = if borrowed { 0 } else { 1 };
                     }
                     0x000E => {
                         // YSHIFT:    8XYE VX = VY << 1
                         // No YSHIFT: 8XYE VX = VX << 1
-                        let shift_src = if self.super_chip_support { X } else { Y };
+                        let shift_src = if self.super_chip_support { x } else { y };
                         self.v[0xF] = self.v[shift_src] >> 7;
-                        self.v[X] = self.v[shift_src] << 1;
+                        self.v[x] = self.v[shift_src] << 1;
                     }
                     _ => {
                         panic!("IMPOSSIBLE NIBBLE!");
@@ -202,31 +203,31 @@ impl Chip8 {
             }
             0x9000 => {
                 // Skip 1 instruction if VX != VY
-                if self.v[X] != self.v[Y] {
+                if self.v[x] != self.v[y] {
                     self.pc += 2;
                 }
             }
             0xA000 => {
                 // I = NNN
-                self.i = NNN;
+                self.i = nnn;
             }
             0xB000 => {
                 // CHIP-8:     0xBNNN Jump to NNN + V0
                 // SUPER-CHIP: 0xBXNN Jump to XNN + VX
-                let v_src = if self.super_chip_support { X } else { 0 };
-                self.pc = NNN + self.v[v_src] as u16;
+                let v_src = if self.super_chip_support { x } else { 0 };
+                self.pc = nnn + self.v[v_src] as u16;
             }
             0xC000 => {
                 // VX = random number bitwise & with NN
                 let random_byte: u8 = (self.rng.next_u32() & 0xFF) as u8;
-                self.v[X] = random_byte & NN;
+                self.v[x] = random_byte & nn;
             }
             0xD000 => {
                 // Alter Display
-                let x_coord = self.v[X] as usize % DISPLAY_WIDTH;
-                let y_coord = self.v[Y] as usize % DISPLAY_HEIGHT;
+                let x_coord = self.v[x] as usize % DISPLAY_WIDTH;
+                let y_coord = self.v[y] as usize % DISPLAY_HEIGHT;
                 self.v[0xF] = 0; // Reset collision flag
-                for index in 0..N as usize {
+                for index in 0..n as usize {
                     let sprite_byte = self.memory[self.i as usize + index];
                     
                     for bit_index in 0..8 {
@@ -249,19 +250,92 @@ impl Chip8 {
 
             }
             0xE000 => {
-                match opcode {
+                match opcode & 0x00FF {
+                    0x009E => {
+                        // Skip next instruction if X key is pressed
+                        if self.keypad[self.v[x] as usize] {
+                            self.pc += 2;
+                        }
+                    }
+                    0x00A1 => {
+                        // Skip next instruction if X key is NOT pressed
+                        if !self.keypad[self.v[x] as usize] {
+                            self.pc += 2;
+                        }
+                    }
                     _ => { println!("Unknown opcode: {:04X}", opcode); }
                 }
             }
             0xF000 => {
-                match opcode {
+                match opcode & 0x00FF {
+                    // Timer Instructions
+                    0x007 => {
+                        // Set VX to current value of Delay Timer
+                        self.v[x] = self.delay_timer;
+                    }
+                    0x015 => {
+                        // Sets the Delay Timer to the value in VX
+                        self.delay_timer = self.v[x];
+                    }
+                    0x018 => {
+                        // Sets the sound timer to the value in VX
+                        self.sound_timer = self.v[x];
+                    }
+                    0x00A => {
+                        // Wait til any button is pressed
+                        let mut key_pressed: bool = false;
+                        for (i, &pressed) in self.keypad.iter().enumerate() {
+                            if pressed {
+                                self.v[x] = i as u8;
+                                key_pressed = true;
+                                break;
+                            }
+                        }
+                        if !key_pressed {
+                            // Don't advance to next instruction
+                            self.pc -= 2;
+                        }
+                    }
+                    0x01E => {
+                        // I = I + VX
+                        let (result, overflow) = self.i.overflowing_add(self.v[x] as u16);
+                        self.i = result;
+                        self.v[0xF] = if overflow { 1 } else { 0 };
+                    }
+                    0x033 => {
+                        // Store number in VX as three decimal digits, and stores in mem at location in reg I
+                        let value = self.v[x];
+                        self.memory[self.i as usize] = value / 100;
+                        self.memory[self.i as usize + 1] = (value % 100) / 10;
+                        self.memory[self.i as usize + 2] = value % 10;
+                    }
+                    0x055 => {
+                        // Store V0-VX variables in memory
+                        for step in 0..=x {
+                            self.memory[self.i as usize + step] = self.v[step];
+                        }
+                        // Original Chip-8 incremented I, but modern don't update I
+                        // if !self.super_chip_support {
+                        //     self.i += x as u16 + 1;
+                        // }
+                    }
+                    0x065 => {
+                        // Loads from memory variables into V0-VX
+                        for step in 0..=x {
+                            self.v[step] = self.memory[self.i as usize + step];
+                        }
+                        // Original Chip-8 incremented I, but modern don't update I
+                        // if !self.super_chip_support {
+                        //     self.i += x as u16 + 1;
+                        // }
+                    }
                     _ => { println!("Unknown opcode: {:04X}", opcode); }
                 }
             }
             _ => {
                 panic!("IMPOSSIBLE NIBBLE!");
             }
-        } 
+        }
         Ok(())
     }
 
